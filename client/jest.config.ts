cfd2b0a0f81fcef811d18657e2d89384625ec707
tgenz1213 <timothy.genz@snhu.edu>
2024-10-18 11:53:08 -0500

    chore: add Jest and Babel for testing

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,8 +1,19 @@
 import type { Config } from "jest";
+import { configDotenv } from "dotenv";
+
+configDotenv({ path: "./.env.development" });
 
 const config: Config = {
+  extensionsToTreatAsEsm: [".ts"],
   verbose: true,
+  testEnvironment: "jest-environment-jsdom",
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  transform: {
+    "^.+.tsx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
 };
 
 export default config;

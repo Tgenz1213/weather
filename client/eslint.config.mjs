542ec323baada757633ff23bacd35f74b02c37bd
tgenz1213 <timothy.genz@snhu.edu>
2025-04-17 18:45:12 -0500

    chore: add rule for requiring aliases instead of relative paths except for .css files

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -2,9 +2,32 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import pathAlias from "eslint-plugin-path-alias";
+import { resolve } from "node:path";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  { ignores: ["**/dist/**", "**/node_modules/**"] },
+  {
+    plugins: {
+      "path-alias": pathAlias,
+    },
+    rules: {
+      "path-alias/no-relative": [
+        "error",
+        {
+          paths: {
+            "@": resolve(import.meta.dirname, "./src"),
+            "@assets": resolve(import.meta.dirname, "./src/assets"),
+            "@components": resolve(import.meta.dirname, "./src/components"),
+            "@hooks": resolve(import.meta.dirname, "./src/hooks"),
+            "@routes": resolve(import.meta.dirname, "./src/routes"),
+            "@services": resolve(import.meta.dirname, "./src/services"),
+          },
+          exceptions: ["*.css"]
+        },
+      ],
+    },
+  },
+  { ignores: ["**/dist/**", "**/node_modules/**"] }
 );
